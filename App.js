3393840766eb179ce6289dd56f8324ba657cc83f
Muhitdin Yahya <monkeydjack0@mail.ru>
2021-09-09 22:44:41 +0200

    first api connection

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from "react";
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const[data, setData] = useState();
+
+  useEffect(() =>{
+    axios.get('https://api.kraken.com/0/public/OHLC?pair=XBTUSD')
+    .then( res =>{
+      const data = res.data.result
+      var name = Object.keys(data)[0]; // Get the first item of the list;  = key name
+      var value = data[name];
+
+
+      setData(value)
+      console.log(data)
+    }).catch(error=> console.log(error))
+  },[])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{data[0][1]}</Text>
       <StatusBar style="auto" />
     </View>
   );
